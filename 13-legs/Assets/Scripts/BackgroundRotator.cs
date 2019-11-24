3f1075198cdc789d95f792abe11c4f10db23db9e
Team-on <timonsol13@gmail.com>
2019-11-25 01:44:20 +0200

    Background

---
+++ 13-legs/Assets/Scripts/BackgroundRotator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRotator : MonoBehaviour {
+	[SerializeField] float speed = 5.0f;
+
+	void Update() {
+		transform.Rotate(0, 0, speed * Time.deltaTime);
+	}
+}
---
+++ 13-legs/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	[SerializeField] float moveForce;
+
+	[SerializeField] Rigidbody2D rb;
+
+	Vector3 moveVect;
+
+	void Update() {
+		moveVect = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+	}
+
+	void FixedUpdate() {
+		rb.AddForce(moveVect * moveForce * Time.fixedDeltaTime);
+	}
+}
