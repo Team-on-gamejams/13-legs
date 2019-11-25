d2d8e1c2574d42ca201c66fa900a3d564013c480
Team-on <timonsol13@gmail.com>
2019-11-25 02:08:13 +0200

    Player hp

--- 13-legs/Assets/Scripts/Player.cs
+++ 13-legs/Assets/Scripts/Player.cs
@@ -3,25 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+	[Header("HP")]
 	[SerializeField] float hpMax;
 	[SerializeField] float hpCurr;
-	[SerializeField] float hpLossPerSec;
+	[SerializeField] float hpLoss;
+	[SerializeField] float hpLossTime;
+	[SerializeField] Transform hpCircle;
 
+	[Header("Moving")]
 	[SerializeField] float moveForce;
 
+	[Header("Refs")]
 	[SerializeField] Rigidbody2D rb;
 
 	Vector3 moveVect;
+	float currHpLoseTimer = 0;
+	bool isPlaying;
+
+	void Start() {
+		StartGame();
+	}
 
 	void Update() {
+		if (!isPlaying)
+			return;
+
 		moveVect = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+		currHpLoseTimer += Time.deltaTime;
+		while (currHpLoseTimer >= hpLossTime) {
+			currHpLoseTimer -= hpLossTime;
+			if ((hpCurr -= hpLoss) <= 0.0f)
+				hpCurr = 0;
+
+			if (hpCurr == 0)
+				Lose();
+
+			hpCircle.localScale = Vector3.one * (hpCurr / hpMax);
+		}
+
 	}
 
 	void FixedUpdate() {
+		if (!isPlaying)
+			return;
+
 		rb.AddForce(moveVect * moveForce * Time.fixedDeltaTime);
 	}
 
-	private void OnTriggerEnter2D(Collider2D collision) {
+	void OnTriggerEnter2D(Collider2D collision) {
 		if(collision.tag == "Food") {
 			Food food = collision.GetComponent<Food>();
 			if((hpCurr += food.hpRegen) > hpMax) 
@@ -29,5 +59,13 @@ public class Player : MonoBehaviour {
 			food.OnEat();
 			Destroy(collision.gameObject);
 		}
+	}
+
+	public void StartGame() {
+		isPlaying = true;
+	}
+
+	public void Lose() {
+		isPlaying = false;
 	}
 }
